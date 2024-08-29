0f1e0763119bf918dbaa59bd2e95e89857becf62
Abinash Patra <abinash.patra@nwbrd.com>
2024-08-29 13:02:03 +0530

    First Commit

---
+++ UpdateSubscriber-Service/src/main/java/com/microservices/update_subscriber/UpdateSubscriberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.update_subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateSubscriberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateSubscriberServiceApplication.class, args);
+	}
+
+}
---
+++ UpdateSubscriber-Service/src/test/java/com/microservices/update_subscriber/UpdateSubscriberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.update_subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateSubscriberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
